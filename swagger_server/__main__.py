c37da3a36b07bce4c66057961e9f7cbcd0387a8a
Youngbok Yoon <master@bluehack.net>
2018-01-21 17:03:12 +0900

    Enabled CORS

--- swagger_server/__main__.py
+++ swagger_server/__main__.py
@@ -2,10 +2,12 @@
 
 import connexion
 from .encoder import JSONEncoder
+from flask_cors import CORS
 
 
 if __name__ == '__main__':
     app = connexion.App(__name__, specification_dir='./swagger/')
+    CORS(app.app)
     app.app.json_encoder = JSONEncoder
     app.add_api('swagger.yaml', arguments={'title': 'This is a API document for Stylens Service'})
     app.run(port=8080)
